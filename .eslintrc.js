aabf418d19c27ae5677574944d6ed1dcfa712d3f
chastephp <chastephp@foxmail.com>
2020-08-28 09:56:14 +0800

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,12 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'vue/no-parsing-error': [2, {
+      'x-invalid-end-tag': false
+    }],
+    'no-undef': 'off',
+    'camelcase': 'off',
+    'eqeqeq': 'off'
   }
 }
